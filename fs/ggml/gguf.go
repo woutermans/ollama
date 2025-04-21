dab5294bfa45bf2788a349ccdef47d39860a0da3
Bruce MacDonald <brucewmacdonald@gmail.com>
2025-04-21 09:43:56 -0700

    patch embeddings

--- fs/ggml/gguf.go
+++ fs/ggml/gguf.go
@@ -530,7 +530,6 @@ func WriteGGUF(ws io.WriteSeeker, kv KV, ts []Tensor) error {
 	})
 
 	var s uint64
-	var alignment int64 = 32
 	for _, t := range ts {
 		t.Offset = s + uint64(ggufPadding(int64(s), int64(alignment)))
 		if err := ggufWriteTensorInfo(ws, t); err != nil {
